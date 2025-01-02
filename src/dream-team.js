497ee3cd915059121cb5ab7e9f08c7e2e5bce7fe
Anton Pivnenko <pivnenkowebdev@gmail.com>
2025-01-02 13:41:14 +0400

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const result = [];
+  for (let i = 0; i < members.length; i += 1) {
+    if (typeof members[i] === 'string') {
+      result.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  return result.sort().join('');
 }
+
 
 module.exports = {
   createDreamTeam
